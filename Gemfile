b8bd61143dc3d9c42bce85dd2d99818c1bd998fe
jcheng <jackie.f.cheng@gmail.com>
2014-03-08 16:05:15 +0800

    add exiftool

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'rails', '3.1.3'
 gem 'mysql2'
 
 gem "paperclip", "~> 4.1"
+gem "mini_exiftool"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,16 @@
+require 'mini_exiftool'
+
 class Photo < ActiveRecord::Base
-  attr_accessible :file
+  attr_accessible :file, :time
   has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
   belongs_to :album
+
+
+  def exif_read
+    p = MiniExiftool.new(self.file.path)
+    puts p[:datetimeoriginal]
+  end
 
 
 end
