4d39c0e790919ed3f6dd1734d4b2535da108f261
mli1 <mli1@successfactors.com>
2014-03-18 18:09:12 +0800

    ui change

--- app/assets/javascripts/album.js
+++ app/assets/javascripts/album.js
@@ -1,20 +1,27 @@
 
 window.onload=function(){
-var imageList2=document.getElementsByClassName('item');
 var imageList=new Array();
-imageList.push("");
-for(var i=0; i<imageList2.length;i++){
-	var temp=imageList2[i];
-	if(temp.height!=4576)
-	imageList.push(temp);
-}
-
-for(var i=1; i<imageList.length;i++){
-	imageList[i].style.height="394px";
-}
+var local=document.location.toString();
+var imageList2=document.getElementsByClassName('item');
 var container = document.getElementById('container');
 var html=[];
 this.listNum=0;
+$.ajax({
+        type : "get", 
+        async:false,
+        url : local, 
+        dataType : "json",
+        success : function(json){ 
+            for(var i=0;i<json.length;i++){
+                 imageList.push(json[i]);
+            }
+        }
+    });
+var hrefInsert="<a href='"+local+"albums/";
+var divString="<div style='width:100%;height:429px;position:relative;'>";
+var divP1String="<div class='p1'>";
+var divP2String="<div class='p2'>";
+var divP3String="<div class='p3'>";
 
 while(this.listNum+1<imageList.length){
 	var flag=imageList.length-this.listNum-1;
@@ -39,6 +46,8 @@ while(this.listNum+1<imageList.length){
                     break;
             case 2: container.innerHTML+=renderhtml_III(this.listNum,html,imageList);
                     break;
+            case 1: container.innerHTML+=renderhtml_IV(this.listNum,html,imageList);
+                    break;
 		}
 	}
  else{
@@ -47,31 +56,56 @@ while(this.listNum+1<imageList.length){
     container.innerHTML+=renderhtml_III(this.listNum,html,imageList);
 }
  }
+function renderhtml_IV(listNum,html,imageList){
+inserthtml= html+divString+divP2String+
+"<figure>"+hrefInsert+imageList[this.listNum+1].id+"/photos'>"+
+  "<img class='image' src='"+imageList[listNum+1].cover_url+"'></a></figure>"+
+  "<header><h1>"+imageList[this.listNum+1].title+"<h1></header></div></div>";
+this.listNum=listNum+1;
+return inserthtml;
+}
 function renderhtml(listNum,html,imageList){
-inserthtml= html+"<div style='width:100%;height:429px'><div class='p1'><figure>"+
-imageList[this.listNum+1].outerHTML+"</figure></div><div class='p2'><figure>"+
-imageList[this.listNum+2].outerHTML+"</figure></div><div class='p1'><figure>"+
-imageList[this.listNum+3].outerHTML+"</figure></div></div>";
+inserthtml= html+divString+divP1String+
+"<figure>"+hrefInsert+imageList[this.listNum+1].id+"/photos'>"+
+   "<img class='image' src='"+imageList[this.listNum+1].cover_url+"'></a></figure>"+
+   "<header><h1>"+imageList[this.listNum+1].title+"</h1></header></div>"+divP2String+
+"<figure>"+hrefInsert+imageList[this.listNum+2].id+"/photos'>"+
+   "<img class='image' src='"+imageList[this.listNum+2].cover_url+"'></a></figure>"+
+   "<header><h1>"+imageList[this.listNum+2].title+"</h1></header></div>"+divP1String+
+"<figure>"+hrefInsert+imageList[this.listNum+3].id+"/photos'>"+
+   "<img class='image' src='"+imageList[this.listNum+3].cover_url+"'></a></figure>"+
+   "<header><h1>"+imageList[this.listNum+3].title+"</h1></header></div></div>";
 this.listNum=listNum+3;
 return inserthtml;
 }
 
 function renderhtml_II(listNum,html,imageList){
-imageList[this.listNum+1].style.height='187px';
-imageList[this.listNum+2].style.height='187px';
-inserthtml= html+"<div style='width:100%;height:429px'><div class='p1'><div class='p3'><figure>"+
-imageList[this.listNum+1].outerHTML+"</figure></div><div class='p3'><figure>"+
-imageList[this.listNum+2].outerHTML+"</figure></div></figure></div><div class='p2'><figure>"+
-imageList[this.listNum+3].outerHTML+"</figure></div><div class='p1'><figure>"+
-imageList[this.listNum+4].outerHTML+"</figure></div></div>";
+/*imageList[this.listNum+1].style.height='187px';
+imageList[this.listNum+2].style.height='187px';*/
+inserthtml= html+divString+divP1String+divP3String+
+"<figure>"+hrefInsert+imageList[this.listNum+1].id+"/photos'>"+
+   "<img class='imageH' src='"+imageList[this.listNum+1].cover_url+"'></a></figure>"+
+   "<header class='headerH'><h1>"+imageList[this.listNum+1].title+"</h1></header></div>"+divP3String+
+"<figure>"+hrefInsert+imageList[this.listNum+2].id+"/photos'>"+
+   "<img class='imageH' src='"+imageList[this.listNum+2].cover_url+"'></a></figure>"+
+   "<header class='headerH'><h1>"+imageList[this.listNum+2].title+"</h1></header></div></figure></div>"+divP2String+
+"<figure>"+hrefInsert+imageList[this.listNum+3].id+"/photos'>"+
+   "<img class='image' src='"+imageList[this.listNum+3].cover_url+"'></a></figure>"+
+   "<header><h1>"+imageList[this.listNum+3].title+"</h1></header></div>"+divP1String+
+"<figure>"+hrefInsert+imageList[this.listNum+4].id+"/photos'>"+
+  "<img class='image' src='"+imageList[this.listNum+4].cover_url+"'></a></figure>"+
+  "<header><h1>"+imageList[this.listNum+4].title+"</h1></header></div></div>";
 this.listNum=listNum+4;
 return inserthtml;
 }
 function renderhtml_III(listNum,html,imageList){
-inserthtml= html+"<div style='width:100%;height:429px'><div class='p2'><figure>"+
-imageList[listNum+1].outerHTML+"</figure></div><div class='p2'><figure>"+
-imageList[listNum+2].outerHTML+"</figure></div></div>";
-this.listNum=listNum+2;
+inserthtml= html+divString+divP2String+
+"<figure>"+hrefInsert+imageList[this.listNum+1].id+"/photos'>"+
+   "<img class='image' src='"+imageList[listNum+1].cover_url+"'></a></figure>"+
+   "<header><h1>"+imageList[this.listNum+1].title+"</h1></header></div>"+divP2String+
+"<figure>"+hrefInsert+imageList[this.listNum+2].id+"/photos'>"+
+   "<img class='image' src='"+imageList[listNum+2].cover_url+"'></a></figure>"+
+   "<header><h1>"+imageList[this.listNum+2].title+"</h1></header></div></div>";this.listNum=listNum+2;
 return inserthtml;
 }
 
